227d600467b4f4c04fdbee2d239c22b22e191cb5
GabrielHorodenski3 <gabrielhorodenski32@gmail.com>
2025-03-29 22:53:18 +0000

    atividade

--- conta.py
+++
@@ -1,3 +0,0 @@
-a, b, c, d, e, f = input(1), int(input(2)), int(int(input(3))), int(int(int(input(4)))), int(int(int(int(input(5))))), int(int(int(int(int(input(6))))))
-resultado = a + (b * c) + ( d* e) + f
-print(resultado)
