ec00d7a0e69b333328074a7929ff9a5fd1fc37a2
GabrielHorodenski3 <gabrielhorodenski32@gmail.com>
2025-03-29 16:12:51 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+flores, n, m = input("Que cor de flor voce deseja"), int(input("Quantas flores voce deseja")), float(input("qual o preço de cada flor"))
+print(flores, n, m)
+print("Total da venda: ${:.2f}".format(n*m))
